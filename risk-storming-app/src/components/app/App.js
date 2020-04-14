cabc786d07da90dd62247612e548cc031889c4ef
Aline L <aline.lanckneus@live.be>
2020-04-14 20:09:54 +0200

    first commit, add dummy json , blue card template

---
+++ risk-storming-app/src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ risk-storming-app/src/components/cards/blue-card/BlueCard.js
@@ -0,0 +1,83 @@
+import blueCards from '../../../assets/en/blueCards.json';
+import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const blueCardList = blueCards;
+const blueCardStyles = makeStyles({
+  root: {
+    maxWidth: 250,
+    backgroundColor: 'rgb(52, 171, 235)',
+    borderRadius: '10px',
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    backgroundColor: 'white',
+    color: 'rgb(52, 171, 235)',
+    border: 'dotted',
+    borderBottomStyle: 'none',
+    paddingLeft: '8px',
+  },
+  subtitle: {
+    fontSize: 14,
+    textTransform: 'uppercase',
+    backgroundColor: 'white',
+    color: 'rgb(52, 171, 235)',
+    border: 'dotted',
+    borderTopStyle: 'none',
+    paddingLeft: '8px',
+  },
+  description: {
+    fontSize: 14,
+    color: 'white',
+    borderTopStyle: 'none',
+    border: '3px dotted white',
+    padding: '8px',
+  },
+  example: {
+    fontSize: 12,
+    color: 'white',
+    padding: '4px',
+  },
+  hr: {
+    border: 'none',
+    borderTop: '3px dotted white',
+  },
+});
+function BlueCard() {
+  const classes = blueCardStyles();
+  return (
+    <React.Fragment>
+      {blueCardList.map((card, i) => (
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title}>{card.title}</Typography>
+            <Typography className={classes.subtitle}>
+              {card.subTitle}
+            </Typography>
+
+            <Typography className={classes.description}>
+              {card.description}
+            </Typography>
+            <Typography className={classes.example}>
+              1 {card.exampleOne}
+            </Typography>
+            <hr className={classes.hr}></hr>
+            <Typography className={classes.example}>
+              2 {card.exampleTwo}
+            </Typography>
+            <hr className={classes.hr}></hr>
+            <Typography className={classes.example}>
+              3 {card.exampleThree}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </React.Fragment>
+  );
+}
+
+export default BlueCard;
