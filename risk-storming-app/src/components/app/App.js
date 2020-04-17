c7982c39d8de0d2b6587bc955b308b54a0508372
Aline Lanckneus <49918534+AlineLanckneus@users.noreply.github.com>
2020-04-17 10:34:50 +0200

    Update App.js

--- risk-storming-app/src/components/app/App.js
+++ risk-storming-app/src/components/app/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'></header>
-      <h1>Hi</h1>
-    </div>
-  );
-}
-
-export default App;
