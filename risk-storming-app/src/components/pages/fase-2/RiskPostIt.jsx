542a411520455030e2b66019f1c50160cdd99d41
Aline L <aline.lanckneus@live.be>
2020-04-17 16:38:28 +0200

    edit post its

--- risk-storming-app/src/components/pages/fase-2/RiskPostIt.jsx
+++ risk-storming-app/src/components/pages/fase-2/RiskPostIt.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 const RiskPostIt = () => {
   return (
     <React.Fragment>
       <div id='riskPostItForm'>
         <Form>
           <Form.Group>
+            <Button variant='outline-dark'>add risk</Button>
             <Form.Control as='textarea' rows='5' />
 
-            <Form.Control as='textarea' rows='5' />
+            <Form.Control className='textBox' as='textarea' rows='5' />
 
-            <Form.Control as='textarea' rows='5' />
+            <Form.Control className='textBox' as='textarea' rows='5' />
 
-            <Form.Control as='textarea' rows='5' />
+            <Form.Control className='textBox' as='textarea' rows='5' />
 
-            <Form.Control as='textarea' rows='5' />
+            <Form.Control className='textBox' as='textarea' rows='5' />
           </Form.Group>
         </Form>
       </div>
@@ -23,3 +25,42 @@ const RiskPostIt = () => {
 };
 
 export default RiskPostIt;
+
+/* 
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import TextBox from './TextBox';
+class RiskPostIt extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAddTextbox = this.handleAddTextbox.bind(this);
+    this.state = { visible: false };
+  }
+  handleAddTextbox() {
+    this.setState({ visible: true });
+    console.log('added textbox');
+  }
+  
+  render() {
+    const visible = this.state.visible;
+    return (
+      <React.Fragment>
+        <div id='riskPostItForm'>
+          <Form>
+            <Form.Group>
+              <Button variant='outline-dark'>add risk</Button>
+
+              <TextBox visible={visible} onClick={this.handleAddTextbox} />
+              <TextBox visible={visible} onClick={this.handleAddTextbox} />
+              <TextBox visible={visible} onClick={this.handleAddTextbox} />
+              <TextBox visible={visible} onClick={this.handleAddTextbox} />
+            </Form.Group>
+          </Form>
+        </div>
+      </React.Fragment>
+    );
+  };
+}
+export default RiskPostIt;
+  */
