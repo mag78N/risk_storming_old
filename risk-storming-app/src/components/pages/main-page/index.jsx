77e9ed3ec9dd7ff2b34af505646e318c568278c7
Madi <magdalena@88em.eu>
2020-04-23 23:19:59 +0200

    Global font-sizes change, main page image added

--- risk-storming-app/src/components/pages/main-page/index.jsx
+++ risk-storming-app/src/components/pages/main-page/index.jsx
@@ -3,11 +3,16 @@ import './index.css';
 import Button from 'react-bootstrap/Button';
 import TopNavbar from '../../TopNavbar/TopNavbar';
 import { A } from 'hookrouter';
+import bg from '../../../assets/images/bg.jpg';
+
 const MainPage = () => {
+  var mainPageStyle = {
+    backgroundImage: `url(${bg})`
+  };
   return (
     <div className='mainPageContainer'>
       <TopNavbar faseNum='Risk Storming Online' />
-      <div>
+      <div className='mainPageJumbotron' style={ mainPageStyle }>
         <A href='/fase1'>
           <Button variant='outline-dark' className='startSessionButton'>Start Session!</Button>
        </A>
