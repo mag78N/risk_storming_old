e2b9733d03fc149502db41b149c9f568538d23b7
Aline L <aline.lanckneus@live.be>
2020-04-30 12:56:54 +0200

    add same bg as textarea to leftpane fase2

--- risk-storming-app/src/components/pages/fase-2/RiskListLeftPane.js
+++ risk-storming-app/src/components/pages/fase-2/RiskListLeftPane.js
@@ -9,19 +9,10 @@ const RiskListLeftPane = (props) => {
     return (
       <div className='riskLane' key={idx}>
         <div className='risk riskPostit' id={`${props.card.id}|risk-${idx}`}>
-          {/*  <textarea
-            onChange={props.handleChange}
-            rows='3'
-            placeholder='enter risk here'
-            name={riskId} 
-            data-id={idx}
-            id={`${props.card.id}|risk-${idx}`}
-            className='risk'
-            resize='none'
-            value={props.riskDetails[idx].riskContent}
-          /> */}
-          <p>{riskId} is:</p>
-          <p>{riskText}</p>
+          <div className='textareaBg'>
+            <p>{riskId} is:</p>
+            <p>{riskText}</p>
+          </div>
         </div>
       </div>
     );
