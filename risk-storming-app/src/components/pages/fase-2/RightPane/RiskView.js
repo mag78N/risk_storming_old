5b6d00242348ab3a4afef1ce3bdd1184b0d989eb
Madi <magdalena@88em.eu>
2020-05-05 15:02:46 +0200

    Add risk form's layout changes

--- risk-storming-app/src/components/pages/fase-2/RightPane/RiskView.js
+++ risk-storming-app/src/components/pages/fase-2/RightPane/RiskView.js
@@ -5,19 +5,22 @@ import React from 'react';
 const RiskView = (props) => {
   return (
     <div className='content'>
-      <form className='riskDetailsForm' onSubmit={props.onsubmit}>
-        <h2 className='riskDetailsHeading'>Add Risks</h2>
+      <form className='riskDetailsForm'>
+        <button
+          onClick={() => props.addNewRow(props.card.id)}
+          type='button'
+          className='btn btn-primary addButton'>
+          <i className='fa fa-plus' aria-hidden='true' />
+          Add risk
+        </button>
         <RiskList
           key={props.key}
-          
-          
           id={props.id}
           card={props.card}
           onchange={props.handleChange}
           addNewRow={props.addNewRow}
           deleteRow={props.deleteRow}
         />
-        <Button onSubmit={props.onsubmit}>Confirm</Button>
       </form>
     </div>
   );
