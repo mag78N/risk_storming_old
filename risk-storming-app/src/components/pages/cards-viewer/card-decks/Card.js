aa4541df40a974f8047dc0d02d754d622bde2cce
Aline L <aline.lanckneus@live.be>
2020-05-20 18:07:03 +0200

    add voting to cardsviewer

--- risk-storming-app/src/components/pages/cards-viewer/card-decks/Card.js
+++ risk-storming-app/src/components/pages/cards-viewer/card-decks/Card.js
@@ -9,9 +9,11 @@ import '../../../cards/Card/Card.css';
 class Card extends React.Component {
   constructor() {
     super();
-    this.state = {};
-   
+    this.state = { clicks: 0 };
   }
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  };
   render() {
     return (
       <div className={`cardContainer ${this.props.color}`}>
@@ -20,25 +22,26 @@ class Card extends React.Component {
           <div className='cardSubtitle'>{this.props.subTitle}</div>
         </div>
         <div className='cardBody'>
+          <a className='cardVote' onClick={this.IncrementItem}>
+            <i class='fa fa-thumbs-up'></i>&nbsp;
+            <span>{this.state.clicks}</span>
+          </a>
           <div className='cardDescription'>{this.props.description}</div>
 
-          
-            <div className='exampleContainer'>
-              <div className='exampleNumber'>1</div>
-              <div className='cardExample1'>{this.props.exampleOne}</div>
-            </div>
+          <div className='exampleContainer'>
+            <div className='exampleNumber'>1</div>
+            <div className='cardExample1'>{this.props.exampleOne}</div>
+          </div>
 
-            <div className='exampleContainer'>
-              <div className='exampleNumber'>2</div>
-              <div className='cardExample2'>{this.props.exampleTwo}</div>
-            </div>
+          <div className='exampleContainer'>
+            <div className='exampleNumber'>2</div>
+            <div className='cardExample2'>{this.props.exampleTwo}</div>
+          </div>
 
-            <div className='exampleContainer'>
-              <div className='exampleNumber'>3</div>
-              <div className='cardExample3'>{this.props.exampleThree}</div>
-            </div>
-         
-          
+          <div className='exampleContainer'>
+            <div className='exampleNumber'>3</div>
+            <div className='cardExample3'>{this.props.exampleThree}</div>
+          </div>
         </div>
       </div>
     );
