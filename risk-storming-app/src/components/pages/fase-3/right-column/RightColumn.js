2fcdb036c9c45565434f350ea068d309d42a7c5a
Aline L <aline.lanckneus@live.be>
2020-05-25 19:05:47 +0200

    add innercardlist f3 for performance optimisation

--- risk-storming-app/src/components/pages/fase-3/right-column/RightColumn.js
+++ risk-storming-app/src/components/pages/fase-3/right-column/RightColumn.js
@@ -1,44 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
-import Card from '../Card';
+import InnerCardlist from './InnerCardList';
+/* import Card from '../Card'; */
 import { Droppable } from 'react-beautiful-dnd';
 import '../styles/fase3.css';
 const CardList = styled.div``;
 
 class RightColumn extends React.Component {
-
   render() {
-      const { colorcards } = this.props;
+    const { colorcards } = this.props;
     return (
-        <Droppable droppableId='RIGHT-COLUMN' direction='vertical' type='CARD'>
+      <Droppable droppableId='RIGHT-COLUMN' direction='vertical' type='CARD'>
         {(provided, snapshot) => (
-           <React.Fragment>
-             <div className='searchBar'>
-          <i className='fa fa-search'></i>
-          <input type='text' onChange={this.props.filtercards} placeholder='search for cards' />
-        </div>
+          <React.Fragment>
+            <div className='searchBar'>
+              <i className='fa fa-search'></i>
+              <input
+                type='text'
+                onChange={this.props.filtercards}
+                placeholder='search for cards'
+              />
+            </div>
             <CardList
               ref={provided.innerRef}
               {...provided.droppableProps}
               isDraggingOver={snapshot.isDraggingOver}
               className={`rightPane`}
             >
-              {colorcards.map((card, index) => (
-                <Card
-                  type='CARD'
-                  key={card['id']}
-                  cardid={card['id']}
-                  card={card}
-                  index={index}
-                />
-              ))}
-            {provided.placeholder}
-            
-          </CardList>
-           </React.Fragment>
-          )}
-        </Droppable>
-     
+              <InnerCardlist colorcards={colorcards} />
+              {provided.placeholder}
+            </CardList>
+          </React.Fragment>
+        )}
+      </Droppable>
     );
   }
 }
